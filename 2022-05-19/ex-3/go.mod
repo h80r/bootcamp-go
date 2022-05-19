1443fcdae0cb250adb97fcba68d48779d63f690f
h80r <h80rgbc@gmail.com>
2022-05-19 15:09:05 -0300

    Finish ex-3

---
+++ 2022-05-19/ex-3/go.mod
@@ -0,0 +1,3 @@
+module ex-3
+
+go 1.18
---
+++ 2022-05-19/ex-3/main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// var 1nome string
+	// Incorreto, não pode ser começado em numérico
+	var nome string
+
+	var sobrenome string
+
+	//var int idade
+	// Incorreto, o nome da variável deve vir antes do tipo
+	var idade int
+
+	//1sobrenome := 6
+	// Incorreto, não pode ser começado em numérico
+	// Além disso, a variável 'sobrenome' já foi definida, e não pode ser declarada novamente utilizando :=
+	sobrenome1 := 6
+
+	var licenca_para_dirigir = true
+
+	// var estatura da pessoa int
+	// Incorreto, não pode conter espaços no nome da variável
+	var estaturaDaPessoa int
+
+	quantidadeDeFilhos := 2
+}
