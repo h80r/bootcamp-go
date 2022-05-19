25e916ea50591d5ef5f8363e8b65ab4d4688328d
h80r <h80rgbc@gmail.com>
2022-05-19 15:16:50 -0300

    Finish ex-4

---
+++ 2022-05-19/ex-4/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var sobrenome string = "Silva"
+
+	// var idade int = "25"
+	// Sendo a variável um Int, o valor recebido deve ser inteiro, e assim está passando string
+	var idade int = 25
+
+	// boolean := "false";
+	// Pelo nome da variável esperamos que seja um boleano, e não a string 'false'
+	boolean := false
+
+	// var salario string = 4585.90
+	// Dado o valor, espera-se que o tipo seja float, e não string
+	var salario = 4585.90
+
+	var nome string = "Fellipe"
+}
